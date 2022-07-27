a01378411d289ca4d872e7614424299eb5d0cdeb
laiozago <laiozago@gmail.com>
2022-07-26 21:40:43 -0300

    adicionei o site mochila de viagem. tem que melhorar posição da imagem do site. tem que add imagem na aba da pagina da mochila e no meu site.

--- js/main.js
+++ js/main.js
@@ -9,13 +9,20 @@ const sites = [{
     projeto: "front-end",
     nome: "Barbearia",
     endereco: "https://barbearia-laiozago.vercel.app/index.html"
+},
+{
+    projeto: "front-end",
+    nome: "Mochila-de-viagem",
+    endereco: "https://mochila-de-viagem-laiozago.vercel.app/"
 }];
 
 let siteRobotron = sites[0].endereco
 let siteBarbearia = sites[1].endereco
+let siteMochilaDeViagem = sites[2].endereco
 
 let cardRobotron = document.querySelector("[data-card='Robotron']")
 let cardBarbearia = document.querySelector("[data-card='Barbearia']")
+let cardMochilaDeViagem = document.querySelector("[data-card='Mochila-de-viagem']")
 
 
 cardRobotron.addEventListener("click", () => {
@@ -24,4 +31,8 @@ cardRobotron.addEventListener("click", () => {
 
 cardBarbearia.addEventListener("click", () => {
     window.open(siteBarbearia)
+})
+
+cardMochilaDeViagem.addEventListener("click", () => {
+    window.open(siteMochilaDeViagem)
 })
